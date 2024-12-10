b6043fc10318f2a91e9ccd9e38ad5e9d7e2af846
Florian Fogliani <florian.fogliani@epita.fr>
2024-12-10 10:26:54 +0100

    12/10/2024-10:26:54

--- TP2/my_pow/my_pow.s
+++ TP2/my_pow/my_pow.s
@@ -1,16 +1,19 @@
     .global main
+
     .text
 
 main:
     mov $5, %r12
     mov $1, %r13
     mov $0, %r14
+    call power
+    ret
 
 power:
     add $1, %r14
     mov $2, %ax
     mul %r13
-    mov bx, %r13
 
-    cmp %r14, %r13
+    cmp %r12, %r13
     jne power
+    ret
